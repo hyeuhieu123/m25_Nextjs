ae5fc7ed9fbf0ebf0aa24d2036f3360a1b595723
hieu <nguyentrunghieuthptxp@gmail.com>
2024-08-20 06:46:09 +0700

    add

---
+++ ss6/app/products/[idProduct]/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+interface Product {
+  idProduct: "string";
+}
+interface Props {
+  param: Product;
+}
+export default function ProductDetail() {
+  return <div>trang chi tiet</div>;
+}
---
+++ ss6/app/products/[idProduct]/review/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+("user");
+export default function page() {
+  console.log("giá trị props");
+  return (
+    <div>
+      <div>trang danh gia san pham</div>
+    </div>
+  );
+}
